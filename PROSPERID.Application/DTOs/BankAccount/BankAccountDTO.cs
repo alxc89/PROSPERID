93aaeedf1b61fbc4f32033ad7c165ecad6d0748e
alxc89 <alexcaardoso@gmail.com>
2023-11-20 22:19:59 -0300

    refector BankAccountDTO

--- PROSPERID.Application/DTOs/BankAccount/BankAccountDTO.cs
+++ PROSPERID.Application/DTOs/BankAccount/BankAccountDTO.cs
@@ -4,26 +4,21 @@ namespace PROSPERID.Application.DTOs.BankAccount;
 
 public class BankAccountDTO
 {
-    public BankAccountDTO(string accountNumber, string accountHolder, decimal balance,
-        List<TransactionDTO> transactions)
+    public BankAccountDTO(Guid id, string accountNumber, string accountHolder, decimal balance)
     {
+        Id = id;
         AccountNumber = accountNumber;
         AccountHolder = accountHolder;
         Balance = balance;
-        Transactions = transactions;
     }
 
+    public Guid Id { get; set; }
     public string AccountNumber { get; set; }
     public string AccountHolder { get; set; }
     public decimal Balance { get; set; }
-    public List<TransactionDTO> Transactions { get; set; } = new List<TransactionDTO>();
 
     public static implicit operator BankAccountDTO(Domain.Entities.BankAccount bankAccount)
     {
-        var listTransactionDTO = new List<TransactionDTO>();
-        foreach (var transaction in bankAccount.Transactions)
-            listTransactionDTO.Add(transaction);
-        return new BankAccountDTO(bankAccount.AccountNumber, bankAccount.AccountHolder, bankAccount.Balance,
-            listTransactionDTO);
+        return new BankAccountDTO(bankAccount.Id, bankAccount.AccountNumber, bankAccount.AccountHolder, bankAccount.Balance);
     }
 }
