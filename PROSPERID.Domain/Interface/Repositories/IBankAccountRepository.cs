81ab0e5f0876ce140f3cf2433dc8b36ca1c9ff8a
alxc89 <alexcaardoso@gmail.com>
2023-11-20 22:21:18 -0300

    refactor interface IBankAccountRepository

--- PROSPERID.Domain/Interface/Repositories/IBankAccountRepository.cs
+++ PROSPERID.Domain/Interface/Repositories/IBankAccountRepository.cs
@@ -6,10 +6,10 @@ public interface IBankAccountRepository
 {
     Task<BankAccount> CreateBankAccountAsync(BankAccount bankAccount);
     Task<IEnumerable<BankAccount>> GetBankAccountsAsync();
-    Task<BankAccount> GetBankAccountByIdAsync(Guid id);
+    Task<BankAccount?> GetBankAccountByIdAsync(Guid id);
     Task<bool> AnyBankAccountAsync(string accountNumber);
-    Task<BankAccount> UpdateBankAccountAsync(BankAccount bankAccount);
-    Task<BankAccount> DeleteBankAccountAsync(Guid id);
+    Task<BankAccount?> UpdateBankAccountAsync(BankAccount bankAccount);
+    Task DeleteBankAccountAsync(Guid id);
     Task<bool> AnyMovementInAccount(string accountNumber);
     Task<bool> VerifyIfExistsAccount(string accountNumber);
 }
