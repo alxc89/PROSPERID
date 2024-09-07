7cb890d57aee076e525ad4d2627d2f0427a7fd01
alxc89 <alexcaardoso@gmail.com>
2024-09-07 00:14:01 -0300

    add BankAccountView class

---
+++ PROSPERID.Application/ModelViews/BankAccount/BankAccountView.cs
@@ -0,0 +1,33 @@
+﻿namespace PROSPERID.Application.ModelViews.BankAccount;
+
+public class BankAccountView(long id, string accountNumber, string accountHolder, decimal balance)
+{
+    /// <summary>
+    /// Id da Conta Bancária
+    /// </summary>
+    /// <example>1</example>
+    public long Id { get; set; } = id;
+
+    /// <summary>
+    /// Numero Conta Bancária
+    /// </summary>
+    /// <example>123456-5</example>
+    public string AccountNumber { get; set; } = accountNumber;
+
+    /// <summary>
+    /// Proprietário da Conta Bancária
+    /// </summary>
+    /// <example>José da Silva</example>
+    public string AccountHolder { get; set; } = accountHolder;
+
+    /// <summary>
+    /// Numero Conta Bancária
+    /// </summary>
+    /// <example>1000.00</example>
+    public decimal Balance { get; set; } = balance;
+
+    public static implicit operator BankAccountView(Core.Entities.BankAccount bankAccount)
+    {
+        return new BankAccountView(bankAccount.Id, bankAccount.AccountNumber, bankAccount.AccountHolder, bankAccount.Balance);
+    }
+}
