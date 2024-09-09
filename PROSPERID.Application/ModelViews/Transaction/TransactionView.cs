0e96d79022b9dba5f4ce845bc44bd983ab640ecc
alxc89 <alexcaardoso@gmail.com>
2024-09-08 22:39:46 -0300

    documenting class TransactionViews

--- PROSPERID.Application/ModelViews/Transaction/TransactionView.cs
+++ PROSPERID.Application/ModelViews/Transaction/TransactionView.cs
@@ -5,12 +5,48 @@ using PROSPERID.Core.Enums;
 namespace PROSPERID.Application.ModelViews.Transaction;
 public class TransactionView
 {
+    /// <summary>
+    /// Id da Transação.
+    /// </summary>
+    /// <example>1</example>
     public long Id { get; set; }
+
+    /// <summary>
+    /// Descrição da Transação
+    /// </summary>
+    /// <example>Mensalidade escolar</example>
     public string Description { get; set; }
+
+    /// <summary>
+    /// Tipo da transação, indica se é pagamento ou recebimento.
+    /// </summary>
+    /// <example>
+    /// ["Pagamento", "Recebimento"]
+    /// </example>
     public TransactionType Type { get; set; }
+
+    /// <summary>
+    /// Valor da transação.
+    /// </summary>
+    /// <example>500.00</example>
     public decimal Amount { get; set; }
+
+    /// <summary>
+    /// Data da Transação.
+    /// </summary>
+    /// <example>2024-09-01</example>
     public DateTime TransactionDate { get; set; }
+
+    /// <summary>
+    /// Data de pagamento da transação.
+    /// </summary>
+    /// <example>2024-09-07</example>
     public DateTime DueDate { get; set; }
+
+    /// <summary>
+    /// Id da Categoria.
+    /// </summary>
+    /// <example>1</example>
     public long CategoryId { get; set; }
 
     public TransactionView(long id, string description, long categoryId, TransactionType type, decimal amount, DateTime transactionDate, DateTime dueDate)
