eb8066ada15e030f465e9e4218dcb778102e04c7
alxc89 <alexcaardoso@gmail.com>
2024-09-08 22:38:42 -0300

    documenting class serviceResponse

--- PROSPERID.Application/Services/Shared/ServiceResponse.cs
+++ PROSPERID.Application/Services/Shared/ServiceResponse.cs
@@ -2,9 +2,25 @@
 
 public class ServiceResponse<T>
 {
+    /// <summary>
+    /// Mensagem de retorno.
+    /// </summary>
     public string Message { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// Status Code.
+    /// </summary>
     public int Status { get; set; } = 400;
+
+    /// <summary>
+    /// Indica se a operação foi sucesso.
+    /// </summary>
+    /// <example>True</example>
     public bool IsSuccess => Status is 200 and <= 299;
+    
+    /// <summary>
+    /// Objeto do retorno.
+    /// </summary>
     public T? Data { get; set; }
 
     protected ServiceResponse() { }
