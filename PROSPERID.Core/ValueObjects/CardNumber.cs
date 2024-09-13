bad8d181c2be9ba74cc0332210d6b4e3c7e8bf78
alxc89 <alexcaardoso@gmail.com>
2024-09-12 23:44:55 -0300

    create valueObject CardNumber and Money

---
+++ PROSPERID.Core/ValueObjects/CardNumber.cs
@@ -0,0 +1,16 @@
+﻿namespace PROSPERID.Core.ValueObjects;
+
+public record CardNumber
+{
+    public string Value { get; }
+
+    public CardNumber(string value)
+    {
+        if (!IsValid(value))
+            throw new ArgumentException("Número de cartão inválido");
+        Value = value;
+    }
+
+    private bool IsValid(string number)
+        => number.Length > 16 && number.All(char.IsDigit);
+}
---
+++ PROSPERID.Core/ValueObjects/Money.cs
@@ -0,0 +1,22 @@
+﻿namespace PROSPERID.Core.ValueObjects;
+
+public record Money
+{
+    public decimal Amount { get; }
+
+    public Money(decimal amount)
+    {
+        Amount = amount;
+    }
+
+    public static Money operator +(Money a, Money b) =>
+        new(a.Amount + b.Amount);
+
+    public static Money operator +(Money a, decimal b) =>
+        new(a.Amount + b);
+
+    public static Money operator -(Money a, Money b) =>
+        new(a.Amount - b.Amount);
+
+    public static implicit operator decimal(Money a) => a.Amount;
+}
