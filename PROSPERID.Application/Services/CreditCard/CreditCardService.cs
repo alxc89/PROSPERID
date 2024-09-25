bba1af62f40de01adda09fbb49ec7369954ed3b2
alxc89 <alexcaardoso@gmail.com>
2024-09-24 23:34:35 -0300

    refactor validateCreditCardInput argument

--- PROSPERID.Application/Services/CreditCard/CreditCardService.cs
+++ PROSPERID.Application/Services/CreditCard/CreditCardService.cs
@@ -1,5 +1,7 @@
 ﻿using PROSPERID.Application.DTOs.CreditCard;
+using PROSPERID.Application.DTOs.Transaction;
 using PROSPERID.Application.ModelViews.CreditCard;
+using PROSPERID.Application.ModelViews.Transaction;
 using PROSPERID.Application.Services.Shared;
 using PROSPERID.Core.Interface.Repositories;
 using PROSPERID.Core.ValueObjects;
@@ -9,32 +11,6 @@ namespace PROSPERID.Application.Services.CreditCard;
 public class CreditCardService(ICreditCardRepository creditCardRepository) : ICreditCardService
 {
     private readonly ICreditCardRepository _repository = creditCardRepository;
-
-    public async Task<ServiceResponse<CreditCardView>> CreateCreditCardAsync(CreateCreditCardDTO createCreditCardDTO)
-    {
-        var validate = ValidateCreditCardInput<CreateCreditCardDTO>.Validate(createCreditCardDTO.Number, createCreditCardDTO.HolderName,
-            createCreditCardDTO.ExpirationDate, createCreditCardDTO.CreditLimit, createCreditCardDTO.DueDate);
-
-        if (validate != null)
-            return ServiceResponseHelper.Error<CreditCardView>(validate.Status, validate.Message);
-
-        if (await _repository.AnyCartCredit(createCreditCardDTO.Number))
-            return ServiceResponseHelper.Error<CreditCardView>(400, "Requisição inválida, Cartão já existente");
-
-        var creditCard = new Core.Entities.CreditCard(new CardNumber(createCreditCardDTO.Number),
-            createCreditCardDTO.HolderName, createCreditCardDTO.ExpirationDate,
-            new Money(createCreditCardDTO.CreditLimit), createCreditCardDTO.DueDate);
-
-        try
-        {
-            CreditCardView createCreditCard = await _repository.CreateCreditCardAsync(creditCard);
-            return ServiceResponseHelper.Success(200, "Cartão criado com sucesso!", createCreditCard);
-        }
-        catch
-        {
-            return ServiceResponseHelper.Error<CreditCardView>(500, "Erro Interno!");
-        }
-    }
 
     public async Task<ServiceResponse<CreditCardView>> GetCreditCardByIdAsync(long id)
     {
@@ -71,9 +47,50 @@ public class CreditCardService(ICreditCardRepository creditCardRepository) : ICr
         }
     }
 
-    public Task<ServiceResponse<CreditCardView>> UpdateCreditCardAsync(long id, UpdateCreditCardDTO updateCreditCardDTO)
+    public async Task<ServiceResponse<CreditCardView>> CreateCreditCardAsync(CreateCreditCardDTO createCreditCardDTO)
     {
-        throw new NotImplementedException();
+        var validate = ValidateCreditCardInput<CreateCreditCardDTO>.Validate(createCreditCardDTO);
+
+        if (validate != null)
+            return ServiceResponseHelper.Error<CreditCardView>(validate.Status, validate.Message);
+
+        if (await _repository.AnyCartCredit(createCreditCardDTO.Number))
+            return ServiceResponseHelper.Error<CreditCardView>(400, "Requisição inválida, Cartão já existente");
+
+        var creditCard = new Core.Entities.CreditCard(new CardNumber(createCreditCardDTO.Number),
+            createCreditCardDTO.HolderName, createCreditCardDTO.ExpirationDate,
+            new Money(createCreditCardDTO.CreditLimit), createCreditCardDTO.DueDate);
+
+        try
+        {
+            CreditCardView createCreditCard = await _repository.CreateCreditCardAsync(creditCard);
+            return ServiceResponseHelper.Success(200, "Cartão criado com sucesso!", createCreditCard);
+        }
+        catch
+        {
+            return ServiceResponseHelper.Error<CreditCardView>(500, "Erro Interno!");
+        }
+    }
+
+    public async Task<ServiceResponse<CreditCardView>> UpdateCreditCardAsync(long id, UpdateCreditCardDTO updateCreditCardDTO)
+    {
+        var validate = ValidateCreditCardInput<UpdateCreditCardDTO>.Validate(updateCreditCardDTO);
+        if (validate != null)
+            return ServiceResponseHelper.Error<CreditCardView>(validate.Status, validate.Message);
+        var creditCard = await _repository.GetCreditCardByIdAsync(id);
+        if (creditCard == null)
+            return ServiceResponseHelper.Error<CreditCardView>(404, "Requisição inválida, Cartão de Crédito não encontrado");
+        creditCard.Update(updateCreditCardDTO.Number, updateCreditCardDTO.HolderName, updateCreditCardDTO.ExpirationDate,
+            updateCreditCardDTO.CreditLimit, updateCreditCardDTO.CurrentBalance, updateCreditCardDTO.DueDate);
+        try
+        {
+            CreditCardView result = await _repository.UpdateCreditCardAsync(creditCard);
+            return ServiceResponseHelper.Success(200, "Cartão de Crédito alterado com sucesso!", result);
+        }
+        catch
+        {
+            return ServiceResponseHelper.Error<CreditCardView>(500, "Erro interno!");
+        }
     }
 
     public Task<ServiceResponse<CreditCardView>> DeleteCreditCardAsync(long id)
--- PROSPERID.Application/Services/CreditCard/ValidateCreditCardInput.cs
+++ PROSPERID.Application/Services/CreditCard/ValidateCreditCardInput.cs
@@ -1,23 +1,22 @@
-﻿using PROSPERID.Application.DTOs.CreditCard;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using PROSPERID.Application.DTOs.CreditCard;
 using PROSPERID.Application.Services.Shared;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PROSPERID.Application.Services.CreditCard;
 
 public static class ValidateCreditCardInput<T>
 {
-    public static ServiceResponse<T> Validate(string number, string holderName, DateTime? expirationDate,
-        decimal creditLimit, DateTime? dueDate)
+    public static ServiceResponse<T> Validate(CreditCardDTO input)
     {
-        if (string.IsNullOrEmpty(number))
+        if (string.IsNullOrEmpty(input.Number))
             return new ServiceResponse<T>("Requisição inválida, Número do Cartão é Obrigatório", 400);
-        if (string.IsNullOrEmpty(holderName))
+        if (string.IsNullOrEmpty(input.HolderName))
             return new ServiceResponse<T>("Requisição inválida, Proprietário do Cartão é Obrigatório", 400);
-        if (expirationDate == null)
+        if (input?.ExpirationDate == null)
             return new ServiceResponse<T>("Requisição inválida, Data de Expiração do Cartão é Obrigatório", 400);
-        if (creditLimit <= 0)
+        if (input.CreditLimit <= 0)
             return new ServiceResponse<T>("Requisição inválida, Limite de crédito do Cartão menor ou igual a 0", 400);
-        if (dueDate == null)
+        if (input?.DueDate == null)
             return new ServiceResponse<T>("Requisição inválida, Data de Vencimento do Cartão é Obrigatório", 400);
 
         return null!;
