e7347410d542bf09f53e87c987ec2594b4396032
alxc89 <alexcaardoso@gmail.com>
2024-09-29 23:04:11 -0300

    creating interface ICreditCardBill

---
+++ PROSPERID.Application/Services/CreditCardBill/ICreditCardBillService.cs
@@ -0,0 +1,14 @@
+﻿using PROSPERID.Application.DTOs.CreditCardBill;
+using PROSPERID.Application.ModelViews.CreditCardBill;
+using PROSPERID.Application.Services.Shared;
+
+namespace PROSPERID.Application.Services.CreditCardBill;
+
+public interface ICreditCardBillService
+{
+    Task<ServiceResponse<CreditCardBillView>> GetCreditCardBillByIdAsync(long id);
+    Task<ServiceResponse<IEnumerable<CreditCardBillView>>> GetCreditCardBillsAsync();
+    Task<ServiceResponse<CreditCardBillView>> CreateCreditCardBillAsync(CreateCreditCardBillDTO createCreditCardBillDTO);
+    Task<ServiceResponse<CreditCardBillView>> UpdateCreditCardBillAsync(long id, UpdateCreditCardBillDTO updateCreditCardBillDTO);
+    Task<ServiceResponse<CreditCardBillView>> DeleteCreditCardBillAsync(long id);
+}
