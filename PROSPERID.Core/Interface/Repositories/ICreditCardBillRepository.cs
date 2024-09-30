250f8f748f24e0c5be5e4e87331e88e68e8ea80b
alxc89 <alexcaardoso@gmail.com>
2024-09-29 23:21:06 -0300

    creating file interface ICreditCardBillRepostory

---
+++ PROSPERID.Core/Interface/Repositories/ICreditCardBillRepository.cs
@@ -0,0 +1,15 @@
+﻿using PROSPERID.Core.Entities;
+using System.Linq.Expressions;
+
+namespace PROSPERID.Core.Interface.Repositories;
+
+public interface ICreditCardBillRepository
+{
+    Task<CreditCardBill> CreateCreditCardBillAsync(CreditCardBill CreditCardBill);
+    Task<IEnumerable<CreditCardBill>> GetCreditCardBillsAsync();
+    Task<CreditCardBill?> GetCreditCardBillByIdAsync(long id);
+    Task<CreditCardBill?> GetCreditCardBillByIdAsync(long id, params Expression<Func<CreditCardBill, object>>[] includes);
+    Task<CreditCardBill> UpdateCreditCardBillAsync(CreditCardBill CreditCardBill);
+    Task DeleteCreditCardBillAsync(CreditCardBill CreditCardBill);
+    Task<bool> AnyCartCredit(string cartNumber);
+}
