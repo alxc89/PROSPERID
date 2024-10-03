ffac4e1dfe87d21bedb0f66445bc19a71f971bda
alxc89 <alexcaardoso@gmail.com>
2024-10-02 21:57:08 -0300

    create implicit operator method for CreditCardBillView

--- PROSPERID.Application/ModelViews/CreditCardBill/CreditCardBillView.cs
+++ PROSPERID.Application/ModelViews/CreditCardBill/CreditCardBillView.cs
@@ -18,4 +18,26 @@ public class CreditCardBillView
     public long? CreditCardId { get; set; }
     public virtual ICollection<TransactionView>? Transactions { get; set; }
 
+    public static implicit operator CreditCardBillView(Core.Entities.CreditCardBill creditCardBill)
+    {
+        ICollection<TransactionView> _transactionView = [];
+
+        if (creditCardBill.Transactions != null)
+            foreach (var bill in creditCardBill.Transactions)
+                _transactionView.Add(bill);
+
+        return new()
+        {
+            BillDate = creditCardBill.BillDate,
+            DueDate = creditCardBill.DueDate,
+            BillingPeriod = creditCardBill.BillingPeriod,
+            CloseDate = creditCardBill.CloseDate,
+            Status = creditCardBill.Status,
+            TotalAmount = creditCardBill.TotalAmount,
+            PaidAmount = creditCardBill.PaidAmount,
+            PaymentStatus = creditCardBill.PaymentStatus,
+            CreditCardId = creditCardBill.CreditCardId,
+            Transactions = _transactionView
+        };
+    }
 }
